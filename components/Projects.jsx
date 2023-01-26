b33b071146a9e52ef0dfa956ffabf230babb039f
Andrew Park <andrewchanwonpark@gmail.com>
2023-01-26 15:42:18 -0800

    updated projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import RawJuicery from "../public/assets/projects/rawJuicery.png";
 import ShipAid from "../public/assets/projects/ShipAid.png";
+import Fleurissent from "../public/assets/projects/fleurissent.png"
 import AYCEHG from "../public/assets/projects/Ayce_Home_and_Garden.png";
 import ProjectItem from "./ProjectItem.jsx";
 
@@ -15,22 +16,22 @@ const Projects = () => {
         <h2 className="py-4"> What I Worked On</h2>
         <div className="grid md:grid-cols-2 gap-8">
           <ProjectItem
-            title="ShipAid"
-            description="Shopify App Development and Integrations"
-            image={ShipAid}
-            url="https://www.shipaid.com/"
-          />
-          <ProjectItem
             title="Raw Juicery"
             description="Shopify Theme Custom Updates"
             image={RawJuicery}
             url="https://rawjuicery.com/"
           />
           <ProjectItem
-            title="AYCE Home & Garden"
-            description="Shopify Store Creation Password: aycecreative"
-            image={AYCEHG}
-            url="https://ayce-home-and-garden.myshopify.com/"
+            title="Fleurissent"
+            description="Shopify Theme Development and Management"
+            image={Fleurissent}
+            url="https://fleurissentskincare.com/"
+          />
+          <ProjectItem
+            title="ShipAid"
+            description="Shopify App Development and Integrations"
+            image={ShipAid}
+            url="https://www.shipaid.com/"
           />
         </div>
       </div>
