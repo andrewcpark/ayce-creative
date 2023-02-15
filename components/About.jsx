56f699ad94787025163a1fc8c0223185ab57cb30
Andrew Park <andrewchanwonpark@gmail.com>
2023-02-15 14:58:59 -0800

    updated about bi

--- components/About.jsx
+++ components/About.jsx
@@ -13,16 +13,16 @@ const About = () => {
             About{" "}
           </p>
           <h2 className="py-4"> Who I Am </h2>
-          <p className="py-2 text-gray-600">
+          <p className="py-2 text-gray-600 text-xl">
             {" "}
-            I specialize in Shopify theme and app development. My journey as
-            Ayce Creative started as a freelance food photographer &
-            videographer hence the name, "All You Can Eat". Since then, I
-            acquired new skills and my passion for helping entrepreneurs
+            I specialize in Shopify theme and app development. My journey with
+            Ayce Creative started as a freelance food photographer and
+            videographer, hence the name "All You Can Eat". Since then, I have
+            acquired new skills and my passion for helping entrepreneurs has
             blossomed into Shopify development. I enjoy working with businesses
             from all industries, helping everyone find success on Shopify. In my
-            free time, I like to travel, train mixed martial arts, and work on
-            personal projects.{" "}
+            free time, I like to travel, train mixed martial arts, and work
+            on personal projects.{" "}
           </p>
           <Link href="/#projects">
             <p className="py-2 text-gray-600 underline cursor-pointer">
